b0ee357ec7bcef5da751af4f76d77c5ef2e4caee
Appari Shanti <64463306+AppariShanti@users.noreply.github.com>
2021-03-18 20:16:56 +0530

    created a test repository

---
+++ switch.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a
+    cin>>a;
+    switch (a)
+    {
+    case a:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
